0866f3ceb90d5b88cd272b5e7f42c69d947d53ed
Praveen <praveenravindran27@gmail.com>
2026-09-14 21:38:43 +0530

    Modified Jenkinsfile for docker build

--- service-discovery/service-discovery/src/main/java/com/ecommerce/service_discovery/ServiceDiscoveryApplication.java
+++ service-discovery/service-discovery/src/main/java/com/ecommerce/service_discovery/ServiceDiscoveryApplication.java
@@ -11,5 +11,5 @@ public class ServiceDiscoveryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
-
+// auto build test
 }
